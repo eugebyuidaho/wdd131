7f4a8d5d7679331df8d16a78fe3e502c7fa3d665
Eugenia Schulz <eschulz@byupathway.edu>
2026-03-31 10:46:08 -0600

    form

---
+++ scripts/review.js
@@ -0,0 +1,50 @@
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+
+const productSelect = document.querySelector("#productName");
+
+if (productSelect) {
+  products.forEach(product => {
+    const option = document.createElement("option");
+    option.value = product.id;
+    option.textContent = product.name;
+    productSelect.appendChild(option);
+  });
+}
+
+const reviewCount = document.querySelector("#reviewCount");
+
+if (reviewCount) {
+  let count = Number(localStorage.getItem("reviewCount")) || 0;
+  count++;
+  localStorage.setItem("reviewCount", count);
+  reviewCount.textContent = count;
+}
+
+document.querySelector("#currentyear").textContent = new Date().getFullYear();
+document.querySelector("#lastModified").textContent = `Last Modified: ${document.lastModified}`;
